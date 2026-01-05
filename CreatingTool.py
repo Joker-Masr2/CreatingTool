990e4c6a3fdcaa34704db25ad22ff6cc3141baa8
Joker-Masr2 <80541593+Joker-Masr2@users.noreply.github.com>
2026-01-06 01:44:18 +0200

    Update CreatingTool.py

--- CreatingTool.py
+++ CreatingTool.py
@@ -4,7 +4,7 @@ import time, random, os, platform, urllib.request, zipfile, threading, sys, asyn
 from packaging import version
 import requests, shutil
 ##update
-__version__ = "2.0"
+__version__ = "2.1"
 REPO = "Joker-Masr2/Updates-"
 
 def check_and_update():
