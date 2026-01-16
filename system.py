5efe5935dba94ac2f9d2c376597b7960b9a4c871
Dark Storm <80541593+Joker-Masr2@users.noreply.github.com>
2026-01-16 18:36:20 +0200

    Remove unnecessary blank line in system.py

--- system.py
+++ system.py
@@ -5,7 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TOOLS_DIR = os.path.join(BASE_DIR, "tools")
 SESS_DIR = os.path.join(BASE_DIR, "sessions")
 os.makedirs(SESS_DIR, exist_ok=True)
-
 started_printed = False
 start_lock = threading.Lock()
 SINGLE_MODE = True
